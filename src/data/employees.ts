e1a77726b53eb31895371713ca91323e72b8d834
arivanan0218 <arivanan_v_e23@engug.ruh.ac.lk>
2025-12-27 17:16:52 +0530

    Finished

--- src/data/employees.ts
+++ src/data/employees.ts
@@ -6,13 +6,13 @@ export interface Employee {
 }
 
 export const employees: Employee[] = [
-  {
+   {
     id: 1,
-    name: "Thiyageswaran Gobishankar",
-    position: "Project Manager",
-    image: "/assets/profile/Gobishankar.jpg"
+    name: "Rajaratnam Sathianathan",
+    position: "Executive Engineer",
+    image: "/assets/profile/sathianathan.jpg"
   },
-  {
+   {
     id: 2,
     name: "Puvanendran Jayanther",
     position: "Technical Officer",
@@ -20,9 +20,9 @@ export const employees: Employee[] = [
   },
   {
     id: 3,
-    name: "Rajaratnam Sathianathan",
-    position: "Executive Engineer",
-    image: "/assets/profile/sathianathan.jpg"
+    name: "Thiyageswaran Gobishankar",
+    position: "Project Manager",
+    image: "/assets/profile/Gobishankar.jpg"
   },
   {
     id: 4,
@@ -32,7 +32,7 @@ export const employees: Employee[] = [
   },
   {
     id: 5,
-    name: "Mrs. Logitha Pantheepan",
+    name: "Mrs. Logitha Partheepan",
     position: "Senior Quantity Surveyor",
     image: "/assets/profile/Logitha.jpg"
   },
@@ -44,7 +44,7 @@ export const employees: Employee[] = [
   },
   {
     id: 7,
-    name: "Sinnarasa Sojithkanth",
+    name: "Sinnarasa Sajithkanth",
     position: "Work Supervisor",
     image: "/assets/profile/Sajithkanth.jpg"
   },
@@ -54,322 +54,202 @@ export const employees: Employee[] = [
     position: "Accountant Assistant",
     image: "/assets/profile/Ninthusha Akka.jpg"
   },
-  {
+   {
     id: 9,
-    name: "Bertin Jude Niloj",
-    position: "Managing Director",
-    image: "/assets/profile/Boss.jpg"
-  },
-  {
-    id: 10,
-    name: "Fawaz",
-    position: "Chief Financial Officer",
-    image: "/assets/profile/Fawaz.jpg"
-  },
-  {
-    id: 11,
-    name: "Hashan",
-    position: "Senior Architect",
-    image: "/assets/profile/Hashan.jpg"
-  },
-  {
-    id: 12,
-    name: "Inzam",
-    position: "Operations Manager",
-    image: "/assets/profile/Inzam.jpg"
-  },
-  {
-    id: 13,
-    name: "Jayalakshan",
-    position: "Team Lead",
-    image: "/assets/profile/Jayalakshan.jpg"
-  },
-  {
-    id: 14,
-    name: "Jhon",
-    position: "Developer",
-    image: "/assets/profile/Jhon2.jpg"
-  },
-  {
-    id: 15,
-    name: "Kajanan",
-    position: "Developer",
-    image: "/assets/profile/Kajanan.jpg"
-  },
-  {
-    id: 16,
-    name: "Kirushna",
-    position: "Developer",
-    image: "/assets/profile/Kirushna.jpg"
-  },
-  {
-    id: 17,
     name: "Luksha",
-    position: "Designer",
+    position: "Accountant Clerk",
     image: "/assets/profile/Luksha.jpg"
   },
-  {
-    id: 18,
-    name: "Maheshika",
-    position: "QA Engineer",
-    image: "/assets/profile/Maheshika.jpg"
-  },
-  {
-    id: 19,
-    name: "Pahirathan",
-    position: "Developer",
-    image: "/assets/profile/Pahirathan.jpg"
-  },
-  {
-    id: 20,
-    name: "Rachsutyan",
-    position: "Trainee",
-    image: "/assets/profile/Rachsutyan.jpg"
-  },
-  {
-    id: 21,
-    name: "Randika",
-    position: "Developer",
-    image: "/assets/profile/Randika.jpg"
-  },
-  {
-    id: 22,
-    name: "Rayan",
-    position: "Developer",
-    image: "/assets/profile/Rayan.jpg"
-  },
-  {
-    id: 23,
-    name: "Sivajathurshana",
-    position: "Developer",
-    image: "/assets/profile/Sivajathurshana.jpg"
-  },
-  {
-    id: 24,
-    name: "Tharshan",
-    position: "Developer",
-    image: "/assets/profile/Tharshan.jpg"
-  },
-  {
-    id: 25,
-    name: "Vinojan",
-    position: "Developer",
-    image: "/assets/profile/Vinojan.jpg"
-  },
-  {
-    id: 26,
-    name: "Arunram",
-    position: "Developer",
-    image: "/assets/profile/Arunram copy.jpg"
-  },
-   {
-    id: 27,
-    name: "Kanushitha",
-    position: "Unwanted Speaker",
-    image: "/assets/profile/Kanushitha.png"
-  },
-  {
-    id: 28,
-    name: "Luksha Sanmugarasa",
-    position: "Accounts Clerk",
-    image: "/assets/profile/dummy.jpg"
-  },
-  {
-    id: 29,
-    name: "Mahroof Fawas Mohamed",
-    position: "Engineering Assistant",
-    image: "/assets/profile/dummy.jpg"
-  },
-  {
-    id: 30,
-    name: "Kulenthran Kulatharshan",
-    position: "Team Member",
-    image: "/assets/profile/dummy.jpg"
-  },
-  {
-    id: 31,
-    name: "Kamalanathan Kajanan",
-    position: "Technical Officer",
-    image: "/assets/profile/dummy.jpg"
-  },
-  {
-    id: 32,
-    name: "Perinparasa Birunthagan",
-    position: "Technical Officer",
-    image: "/assets/profile/dummy.jpg"
-  },
-  {
-    id: 33,
-    name: "Balachandran Shwethaan",
-    position: "Technical Officer",
-    image: "/assets/profile/dummy.jpg"
-  },
-  {
-    id: 34,
-    name: "Ahilan Pahirathan",
-    position: "Engineering Draughtsman",
-    image: "/assets/profile/dummy.jpg"
-  },
-  {
-    id: 35,
-    name: "Eng. Subendran Jayalakshan",
-    position: "Engineer",
-    image: "/assets/profile/dummy.jpg"
-  },
-  {
-    id: 36,
+    {
+    id: 10,
     name: "Akash Amalathas",
     position: "Senior Quantity Surveyor",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 37,
-    name: "Perinparasa Vinojan",
-    position: "Project Manager",
-    image: "/assets/profile/dummy.jpg"
+   {
+    id: 11,
+    name: "Pahirathan",
+    position: "Draughtsman",
+    image: "/assets/profile/Pahirathan.jpg"
   },
-  {
-    id: 38,
+    {
+    id: 12,
+    name: "Vinojan",
+    position: "Project Manager",
+    image: "/assets/profile/Vinojan.jpg"
+  },
+   {
+    id: 13,
+    name: "Kajanan",
+    position: "Technical Officer",
+    image: "/assets/profile/Kajanan.jpg"
+  },
+   {
+    id: 14,
     name: "N. M. Musaib",
     position: "Engineering Assistant",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 39,
+   {
+    id: 15,
     name: "M.T.M. Aathil",
     position: "Assistant Engineer",
     image: "/assets/profile/dummy.jpg"
   },
   {
-    id: 40,
+    id: 16,
     name: "Dafni Ockersz",
     position: "Quantity Surveyor",
     image: "/assets/profile/dummy.jpg"
   },
   {
-    id: 41,
+    id: 17,
     name: "M.H.D. Farhan",
     position: "Quantity Surveyor",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 42,
+   {
+    id: 18,
+    name: "Perinparasa Birunthagan",
+    position: "Technical Officer",
+    image: "/assets/profile/dummy.jpg"
+  },
+   {
+    id: 19,
+    name: "Suveethan",
+    position: "Technical Officer",
+    image: "/assets/profile/dummy.jpg"
+  },
+   {
+    id: 20,
     name: "A. Anistraj",
     position: "Engineering Assistant",
     image: "/assets/profile/dummy.jpg"
   },
   {
-    id: 43,
+    id: 21,
     name: "Afra",
     position: "Trainee",
-    image: "/assets/profile/dummy.jpg"
+    image: "/assets/profile/afra.jpeg"
   },
-  {
-    id: 44,
+   {
+    id: 22,
     name: "Thurshika",
-    position: "Team Member",
-    image: "/assets/profile/dummy.jpg"
+    position: "",
+    image: "/assets/profile/thursi.jpeg"
   },
   {
-    id: 45,
+    id: 23,
     name: "Keishman",
-    position: "Team Member",
-    image: "/assets/profile/dummy.jpg"
+    position: "",
+    image: "/assets/profile/keishman.PNG"
   },
   {
-    id: 46,
+    id: 24,
     name: "Aththaf",
-    position: "Team Member",
+    position: "",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 47,
+   {
+    id: 25,
     name: "Samith",
-    position: "Team Member",
+    position: "",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 48,
+   {
+    id: 26,
     name: "Mathushanka",
     position: "Quantity Surveyor",
-    image: "/assets/profile/dummy.jpg"
+    image: "/assets/profile/mathu.jpeg"
   },
-  {
-    id: 49,
+   {
+    id: 27,
     name: "Faslul",
-    position: "Team Member",
+    position: "",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 50,
+   {
+    id: 28,
     name: "Sanathani",
     position: "Trainee",
-    image: "/assets/profile/dummy.jpg"
+    image: "/assets/profile/sana.jpeg"
   },
-  {
-    id: 51,
+   {
+    id: 29,
     name: "Hishobanath",
-    position: "Team Member",
-    image: "/assets/profile/dummy.jpg"
+    position: "",
+    image: "/assets/profile/kisobanath.jpeg"
   },
-  {
-    id: 52,
+    {
+    id: 30,
     name: "Pulenthirathas Manoraj",
     position: "Quantity Surveyor",
-    image: "/assets/profile/dummy.jpg"
+    image: "/assets/profile/mano.jpeg"
   },
   {
-    id: 53,
+    id: 31,
     name: "Janarthanan",
     position: "Project Manager",
-    image: "/assets/profile/dummy.jpg"
+    image: "/assets/profile/janarthanan.jpeg"
   },
-  {
-    id: 54,
+   {
+    id: 32,
     name: "Thayalanesan Titus",
     position: "Project Manager",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 55,
+   {
+    id: 33,
     name: "Pallavi",
     position: "Trainee",
-    image: "/assets/profile/dummy.jpg"
+    image: "/assets/profile/pallavi.jpeg"
+  },
+   {
+    id: 34,
+    name: "Pavalagsana",
+    position: "Trainee",
+    image: "/assets/profile/ak.jpeg"
   },
   {
-    id: 56,
+    id: 35,
     name: "Rishoban",
     position: "Trainee",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 57,
-    name: "Pavalagsana",
-    position: "Trainee",
-    image: "/assets/profile/dummy.jpg"
-  },
-  {
-    id: 58,
+    {
+    id: 36,
     name: "Farsan",
-    position: "Team Member",
+    position: "",
     image: "/assets/profile/dummy.jpg"
   },
   {
-    id: 59,
+    id: 37,
     name: "Sajeeth",
     position: "Trainee",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 60,
-    name: "Dinuksahan",
-    position: "Team Member",
+    {
+    id: 38,
+    name: "Fawaz",
+    position: "Engineering Assistant",
+    image: "/assets/profile/Fawaz.jpg"
+  },
+   {
+    id: 39,
+    name: "Krismitha",
+    position: "",
     image: "/assets/profile/dummy.jpg"
   },
-  {
-    id: 61,
-    name: "Krismitha",
-    position: "Team Member",
+    {
+    id: 40,
+    name: "Dolashna",
+    position: "",
+    image: "/assets/profile/dummy.jpg"
+  },
+    {
+    id: 41,
+    name: "Sangeerthanan",
+    position: "",
     image: "/assets/profile/dummy.jpg"
   }
 ];
